cf4c8e0f37c74b24cdc4705ddefb45cf33c7ce10
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-12 10:48:29 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,8 @@
 class Project 
-attr_accessor :name, :title 
+attr_accessor :title, :backers  
 
-def initialize(name)
-  @name = name 
+def initialize(title)
+  @title = title 
   @backers = []
 end 
 
